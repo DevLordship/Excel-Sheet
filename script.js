b905a12c8872f5233636943388dade8b5a1dd7e2
DevLordship <devlordship@gmail.com>
2024-01-03 12:10:32 +0530

    finished tthe ui and made the address feature

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let addressColumnContainer = document.querySelector(
     ".address-column-container"
 );
 let addressRowContainer = document.querySelector(".address-row-container");
+let cellscontainer = document.querySelector(".cells-container");
+let addressbar = document.querySelector(".address-bar");
 
 for (let i = 0; i < rows; i++) {
     let addressCol = document.createElement("div");
@@ -19,4 +21,25 @@ for (let i = 0; i < colums; i++) {
     addressRow.setAttribute("class", "address-row");
     addressRow.innerHTML = String.fromCharCode(65 + i);
     addressRowContainer.appendChild(addressRow);
+}
+
+for (let i = 0; i < rows; i++) {
+    let rowContainer = document.createElement("div");
+    rowContainer.setAttribute("class", "row-conatiner");
+    for (let j = 0; j < colums; j++) {
+        let cell = document.createElement("div");
+        cell.setAttribute("class", "cell");
+        cell.setAttribute("contenteditable", "true");
+        addListenerForAddressBarDisplay(cell, i, j);
+        rowContainer.appendChild(cell);
+    }
+    cellscontainer.appendChild(rowContainer);
+}
+
+function addListenerForAddressBarDisplay(cell, i, j) {
+    cell.addEventListener("click", (e) => {
+        let rowID = i + 1;
+        let colID = String.fromCharCode(65 + j);
+        addressbar.value = `${colID}${rowID}`;
+    });
 }
